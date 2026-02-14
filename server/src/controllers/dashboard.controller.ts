ac32e609724c0fd90027e9e8758c7e8e0ea52414
avitzen <dvltinv@gmail.com>
2026-02-14 09:08:06 +0500

    feat: add endpoint to retrieve dashboard statistics data for average check price, daily orders, user counts, and sales.

--- server/src/controllers/dashboard.controller.ts
+++ server/src/controllers/dashboard.controller.ts
@@ -54,3 +54,94 @@ export const getDashboardData = async (req: Request, res: Response, next: NextFu
         next(error);
     }
 }
+
+
+export const getStatisticsData = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const dayRange = req.query.dayRange as string;
+
+        const day = new Date();
+        const threeMonthsAgo = new Date(day.getDate() - Number(dayRange));
+
+        // callculate average check price
+        // calculate daily average orders price
+        // callculate last months total orders number
+        // calculate new users number for a last months
+        // calculate sales per day by order number
+        // calculate the most sold sizes and colors
+        // calculate the most sold products
+
+        const averageCheckPrice = await prisma.orders.aggregate({
+            _avg: {
+                total_price: true,
+            },
+            where: {
+                createdAt: {
+                    gte: threeMonthsAgo
+                }
+            }
+        });
+
+        const dailyAverageOrdersPrice = await prisma.orders.aggregate({
+            _sum: {
+                total_price: true,
+            },
+            where: {
+                createdAt: {
+                    gte: threeMonthsAgo
+                }
+            }
+        });
+
+        const monthsOrdersNumber = await prisma.orders.count({
+            where: {
+                createdAt: {
+                    gte: threeMonthsAgo
+                }
+            }
+        });
+
+        const newUserCount = await prisma.user.count({
+            where: {
+                createdAt: {
+                    gte: threeMonthsAgo
+                }
+            }
+        });
+
+        const week = 7;
+        const month = 30;
+
+        const weekSales = [];
+        const monthSales = [];
+
+        for (let i = 0; i < week; i++) {
+            const weekDay = new Date(day.getDate() - i);
+            const weekDayOrders = await prisma.orders.count({
+                where: {
+                    createdAt: {
+                        gte: weekDay,
+                        lte: weekDay
+                    }
+                }
+            });
+            weekSales.push(weekDayOrders);
+        };
+
+        for (let i = 0; i < month; i++) {
+            const monthDay = new Date(day.getDate() - i);
+            const monthDayOrders = await prisma.orders.count({
+                where: {
+                    createdAt: {
+                        gte: monthDay,
+                        lte: monthDay
+                    }
+                }
+            });
+            monthSales.push(monthDayOrders);
+        };
+
+    } catch (error) {
+        next(error);
+    }
+}
